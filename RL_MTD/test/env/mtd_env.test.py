75ec0163d232204c01f1d5c21608e8e5214b6678
cailllev <cailllev@students.zhaw.ch>
2020-11-03 18:18:15 +0100

    add test for model (gym conformity)

---
+++ RL_MTD/test/env/mtd_env.test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.env.mtd_env import MTDEnv
+from stable_baselines.common.env_checker import check_env
+
+
+class DetectionSystemTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.env = MTDEnv()
+
+    def test_check_env(self):
+        check_env(self.env)
+
+
+if __name__ == '__main__':
+    unittest.main()
