f1dd9394ebd68eaa10574ce5c1d70e27053ae05e
cailllev <cailllev@students.zhaw.ch>
2020-11-04 16:05:17 +0100

    refactor rl_mtd, save results in file, add options (in config section) to learn or not and to simulate only best learned algo or all algos

--- RL_MTD/src/rl_mtd.py
+++ RL_MTD/src/rl_mtd.py
@@ -1,42 +1,81 @@
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines import PPO2
+from stable_baselines import PPO2, ACKTR, A2C, SAC, TD3
 
 from src.env.mtd_env import MTDEnv
 
-# ------------------------ learning ------------------------ #
+# RL Algos
+#   Model Free
+#       Policy Optimization
+#           ACKTR
+#           A2C
+#           PP02
+#       Q-Learning
+#           * not usefull as only partially observable model (agent does not "know" the state)
+#       Hybrid
+#           SAC
+#           TD3
+#   Model Based
+#       * no support from stable_baselines
+
+
+# ------------------------- config ------------------------- #
 learn = False
+simulate_only_best = True
+results_file = "results.txt"
+
+
+# ------------------------ learning ------------------------ #
+def get_best_algorithm():
+    # type: () -> str
+    try:
+        file = open(results_file)
+        last_line = None
+        for line in file:
+            last_line = line
+        file.close()
+
+        return last_line.split(" ")[0]
+
+    except FileNotFoundError:
+        return "Random"
+
 
 env = MTDEnv()
 model = PPO2(MlpPolicy, env, verbose=0)
+algorithms = [PPO2, ACKTR, A2C, SAC, TD3]
 
 if learn:
-    import time
-    start = time.time()
-    timesteps = 10**6
+    for algorithm in algorithms:
+        import time
 
-    print("Learing...")
-    print(f"{timesteps} steps to simulate.")
-    print(f"Estimated time: {timesteps / 10**5} min.")
+        start = time.time()
+        timesteps = 10 ** 6
 
-    # crazy magic here
-    model.learn(total_timesteps=timesteps)
-    model.save("PPO2_Agent")
+        print(f"Learing {algorithm}...")
+        print(f"{timesteps} steps to simulate.")
+        print(f"Estimated time: {timesteps / 10 ** 5} min.")
 
-    print(f"{time.time() - start} sec to simulate {timesteps} steps")
+        # crazy magic here
+        model.learn(total_timesteps=timesteps)
+        model.save(algorithm)
 
-else:
-    model.load("PPO2_Agent")
+        print(f"{time.time() - start} sec to simulate {timesteps} steps")
 
 # ------------------------ evaluating ------------------------ #
+best_avg_steps = [0, "Random"]
 show_model_after_each_step = False
 show_results_after_each_sim = False
 
-simulations_count = 200
+simulations_count = 100
 
-sim_types = ["RL", "Random", "Static"]
+if simulate_only_best:
+    sim_types = [get_best_algorithm(), "Random", "Static"]
+else:
+    sim_types = [*algorithms, "Random", "Static"]
+
 results = dict.fromkeys(sim_types)
 # {
-#   "RL": {
+#   "PP02": {
 #       "steps": List[int],
 #       "total_reward": List[int],
 #       "avg_steps": float,
@@ -46,13 +85,14 @@ results = dict.fromkeys(sim_types)
 #   ...
 # }
 
-print("*********************************************************************")
-print(f"Starting Simulation Types: {', '.join(sim_types)}")
-print(f"Simulations per Type:      {simulations_count}")
+f = open(results_file, "w")
+f.write("*********************************************************************\n")
+f.write(f"Starting Simulation Types: {', '.join(sim_types)}\n")
+f.write(f"Simulations per Type:      {simulations_count}\n")
 
 for sim_type in sim_types:
-    print("*********************************************************************")
-    print("Simulation Type: " + sim_type)
+    f.write("*********************************************************************\n")
+    f.write(f"Simulation Type: {sim_type}\n")
 
     min_steps = env.get_simulation_steps()
     max_steps = 0
@@ -60,6 +100,7 @@ for sim_type in sim_types:
     results[sim_type] = {}
     results[sim_type]["steps"] = []
     results[sim_type]["total_reward"] = []
+    results[sim_type]["defender_wins"] = 0
 
     null_action = [0, 0]
 
@@ -71,12 +112,12 @@ for sim_type in sim_types:
         while not done:
 
             # select action according to sim_type
-            if sim_type is "RL":
-                action, _ = model.predict(obs)
-            elif sim_type is "Random":
+            if sim_type is "Random":
                 action = env.action_space.sample()
-            else:
+            elif sim_type is "Static":
                 action = null_action
+            else:
+                action, _ = model.predict(obs)
 
             # do action on model
             obs, rewards, done, info = env.step(action)
@@ -85,6 +126,9 @@ for sim_type in sim_types:
 
         results[sim_type]["steps"].append(env.get_counter())
         results[sim_type]["total_reward"].append(env.get_total_reward() / env.get_counter())
+
+        if env.defender_wins():
+            results[sim_type]["defender_wins"] += 1
 
         if env.get_counter() < min_steps:
             min_steps = env.get_counter()
@@ -106,11 +150,29 @@ for sim_type in sim_types:
 
     avg_steps = round(sum_steps / simulations_count, 3)
     avg_reward = round(sum_rewards / simulations_count, 3)
+
+    if avg_steps > best_avg_steps[0]:
+        best_avg_steps = [avg_steps, sim_type]
+
     results[sim_type]["avg_steps"] = avg_steps
     results[sim_type]["avg_reward"] = avg_reward
 
-    print("")
-    print(f"Avg steps:        {avg_steps}")
-    print(f"Avg reward/step: {avg_reward}")
-    print(f"Min steps:        {min_steps}")
-    print(f"Max steps:        {max_steps}")
+    defender_wins = results[sim_type]['defender_wins']
+    attacker_wins = simulations_count - defender_wins
+
+    f.write(f"*****************{'*' * len(sim_type)}\n")
+    f.write(f"Avg steps:        {avg_steps}\n")
+    f.write(f"Avg reward/step: {avg_reward}\n")
+    f.write(f"Min steps:        {min_steps}\n")
+    f.write(f"Max steps:        {max_steps}\n\n")
+    f.write(f"Defender wins:    {defender_wins}\n")
+    f.write(f"Attacker wins:    {attacker_wins}\n")
+
+f.write("*********************************************************************\n")
+f.write(f"{best_avg_steps[1]} is the best algorithm with {best_avg_steps[0]} avg steps.\n")
+f.close()
+
+f = open(results_file, "r")
+s = "".join(f.readlines())
+print(s)
+f.close()
